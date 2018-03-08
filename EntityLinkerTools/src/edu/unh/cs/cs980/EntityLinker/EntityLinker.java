92107e185c192a9dad124f2c62c65dbde0f677af
Nithin Sivakumar <nithinsivakumar@outlook.com>
2018-03-08 15:44:57 -0500

    Working with DBpedia linker

--- EntityLinkerTools/src/edu/unh/cs/cs980/EntityLinker/EntityLinker.java
+++ EntityLinkerTools/src/edu/unh/cs/cs980/EntityLinker/EntityLinker.java
@@ -1,5 +1,64 @@
 package edu.unh.cs.cs980.EntityLinker;
 
-public class EntityLinker {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.dbpedia.spotlight.exceptions.AnnotationException;
+import org.dbpedia.spotlight.model.DBpediaResource;
+import org.dbpedia.spotlight.model.Text;
+
+import edu.unh.cs.cs980.EntityTools.DbpediaSpotlightLinker;
+import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.Data.PageSkeleton;
+import edu.unh.cs.treccar_v2.Data.Section;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+
+public class EntityLinker {
+	
+    private static void usage() {
+        System.out.println("Command line parameters: paragraphID+contents_file");
+        System.exit(-1);
+    }
+	
+	private static void readLeadParagraphID(String paraFile) throws FileNotFoundException
+	{
+		
+        final FileInputStream fileInputStream2 = new FileInputStream(new File(paraFile));
+//        final Iterator<Data.Paragraph> paragraphIterator = DeserializeData.iterParagraphs(fileInputStream2);
+//        for (int i=1; paragraphIterator.hasNext(); i++)
+//        {
+//        	final String paraId = paragraphIterator.next().getParaId();
+//        	final String paraText = paragraphIterator.next().getTextOnly();
+//        	//final List<String> paraEnities = paragraphIterator.next().getEntitiesOnly();
+//        	System.out.println(i + " " + paraId + " - " + paraText );
+//        }
+        
+        
+        for(Data.Paragraph p : DeserializeData.iterableParagraphs(fileInputStream2))
+        {
+        	final String paraId = p.getParaId();
+        	final String paraText = p.getTextOnly();
+        	final List<String> paraEnities = p.getEntitiesOnly();
+        	System.out.println( paraId + " - " + paraText + " - " + paraEnities.toString());
+        }
+	}
+	
+	 public static void main(String[] args) throws FileNotFoundException, AnnotationException{
+		System.setProperty("file.encoding", "UTF-8");
+
+//        if (args.length < 1)
+//            usage();
+		
+		final String paragraphsFile = args[0];
+		//readLeadParagraphID(paragraphsFile);
+		
+		DbpediaSpotlightLinker c = new DbpediaSpotlightLinker ();
+		String text = "Prior to the German invasion of Norway on 9 April 1940, the Deuxième Bureau (French military intelligence) removed  of heavy water from the plant in Vemork in then-neutral Norway. The plant's managing director, Aubert, agreed to lend the heavy water to France for the duration of the war. The French transported it secretly to Oslo, to Perth, Scotland, and then to France. The plant remained capable of producing heavy water.";
+		List<DBpediaResource> response = c.extract(new Text(text));
+		System.out.println(response);
+	}
 }
---
+++ EntityLinkerTools/src/edu/unh/cs/cs980/EntityTools/DbpediaSpotlightLinker.java
@@ -0,0 +1,89 @@
+package edu.unh.cs.cs980.EntityTools;
+
+import org.apache.lucene.document.Document;
+import org.json.*;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.*;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.methods.GetMethod;
+import java.net.URLEncoder;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.dbpedia.spotlight.exceptions.AnnotationException;
+import org.dbpedia.spotlight.model.DBpediaResource;
+import org.dbpedia.spotlight.model.Text;
+
+public class DbpediaSpotlightLinker extends AnnotationClient{
+	
+	private final static String API_URL = "http://spotlight.dbpedia.org/";
+	private static final double CONFIDENCE = 0.2;
+	private static final int SUPPORT = 0;
+	
+	String para = "";
+
+
+	
+	
+
+
+	@Override
+	public List<DBpediaResource> extract(Text text) throws AnnotationException {
+
+		LOG.info("Querying url");
+		
+		String spotlightResponse;
+		try {
+			GetMethod getMethod = new GetMethod(API_URL + "rest/annotate/?" +
+					"confidence=" + CONFIDENCE
+					+ "&support=" + SUPPORT
+					+ "&text=" + URLEncoder.encode(text.text(), "utf-8"));
+			getMethod.addRequestHeader(new Header("Accept", "application/json"));
+
+			spotlightResponse = request(getMethod);
+		} catch (UnsupportedEncodingException e) {
+			throw new AnnotationException("Could not encode text.", e);
+		}
+
+		assert spotlightResponse != null;
+
+		JSONObject resultJSON = null;
+		JSONArray entities = null;
+		
+		try {
+			resultJSON = new JSONObject(spotlightResponse);
+			entities = resultJSON.getJSONArray("Resources");
+		} catch (JSONException e) {
+			throw new AnnotationException("Received invalid response from DBpedia Spotlight API.");
+		}
+		
+		
+		LinkedList<DBpediaResource> resources = new LinkedList<DBpediaResource>();
+		for(int i = 0; i < entities.length(); i++) {
+			try {
+				JSONObject entity = entities.getJSONObject(i);
+				resources.add(
+						new DBpediaResource(entity.getString("@URI"),
+								Integer.parseInt(entity.getString("@support"))));
+
+			} catch (JSONException e) {
+                LOG.error("JSON exception "+e);
+            }
+
+		}
+
+
+		return resources;
+	}
+	
+		
+
+	
+	
+	
+	
+
+}
