32809b38d74db94f6bc4006ae77a8f55b90a4d9f
Nithin Sivakumar <nithinsivakumar@outlook.com>
2018-03-11 14:11:48 -0400

    Changes

--- EntityLinkerTools/src/edu/unh/cs/cs980/Evaluator/F1Measure.java
+++ EntityLinkerTools/src/edu/unh/cs/cs980/Evaluator/F1Measure.java
@@ -7,64 +7,44 @@ import java.util.Map.Entry;
 
 public class F1Measure {
 
-	public List<Double> evaluateMeasure(Map<String, List<String>> groundTruth, Map<String, List<String>> dBpediaentries) {
+	public List<Double> evaluateMeasure(Map<String, List<String>> groundTruth,
+			Map<String, List<String>> dBpediaentries) {
 		// TODO Auto-generated method stub
 		double f1Score = 0.0;
+		int entityRetireved = 0;
+		int entityRelevant = 0;
+		int relevantEntityRetrieved = 0;
+		// int noOfCorrectEntities = 0;
 		List<Double> F1ScoreList = new ArrayList<Double>();
 		for (Entry<String, List<String>> iter : groundTruth.entrySet()) {
 			List<String> trueEntities = new ArrayList<String>();
 			List<String> retrievedEntities = new ArrayList<String>();
 
-			
 			// Get the values of truth and retrieved to commpare the reuslt
 			trueEntities = iter.getValue();
 			retrievedEntities = dBpediaentries.get(iter.getKey());
-			int noOfCorrectEntities = 0;
 
+			entityRetireved += trueEntities.size();
+			entityRelevant += retrievedEntities.size();
 			for (int i = 0; i < retrievedEntities.size(); i++) {
-				if (trueEntities.contains(retrievedEntities.get(i)))
-				{
-					System.out.println("cntains");
-					noOfCorrectEntities++;
+				if (trueEntities.contains(retrievedEntities.get(i))) {
+					relevantEntityRetrieved++;
 				}
-				
-				System.out.println(noOfCorrectEntities);
 
 			}
-			
-			
-			
-			if( (trueEntities.size() == 0) && (retrievedEntities.size() == 0))
-			{
-				f1Score = 1.0;
-			}
-			
-			else if ((trueEntities.size() == 0) && (retrievedEntities.size() != 0)) {
-				f1Score = 0.0;
-			}
-			
-			else if ((trueEntities.size() != 0) && (retrievedEntities.size() == 0)) {
-				f1Score = 0.0;
-			}
-			
-			else
-			{
-				double r = noOfCorrectEntities / trueEntities.size();
-				double p = noOfCorrectEntities / retrievedEntities.size();
-				
-				if((r == 0) && (p == 0))
-				{
-					f1Score = 0.0;
-				}
-				else
-				{
-					f1Score = ( 2 * p * r) / (r + p) ;
-				}
-			}
-			F1ScoreList.add(f1Score);
-			
 
 		}
+
+		double retrievedEntities = (double) entityRelevant / groundTruth.size();
+		double relevantEntities = (double) entityRetireved / groundTruth.size();
+		double entitiesRetrievdRelevant = (double) (relevantEntityRetrieved / groundTruth.size());
+
+		double r = entitiesRetrievdRelevant /retrievedEntities;
+		double p = entitiesRetrievdRelevant / relevantEntities;
+
+		f1Score = (2 * p * r) / (r + p);
+
+		F1ScoreList.add(f1Score);
 
 		return F1ScoreList;
 	}
